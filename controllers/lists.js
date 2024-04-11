097b24c7cbdc0289195773ebcccf2b37a06ee816
Jason Nguyen <j1.nguyen@yahoo.com>
2024-04-11 13:00:57 -0700

    users can add books to list

--- controllers/lists.js
+++ controllers/lists.js
@@ -6,8 +6,36 @@ const User = require('../models/user')
 module.exports = {
     index,
     new: newList,
-    create
+    create,
+    show,
+    addToList
 };
+
+async function addToList(req, res) {
+    const list = await List.findById(req.params.listId);
+    list.books.push(req.body.bookId);
+    try {
+        await list.save();
+        res.redirect(`/${req.user._id}/lists/${list._id}`)
+    } catch (error) {
+        console.error('Error adding to list:' , error);
+    }
+}
+
+async function show(req, res) {
+    try {
+        const list = await List.findById(req.params.listId).populate('books');
+        const books = await Book.find({ _id: { $nin: list.books } });
+        const booksWithAuthors = await Promise.all(books.map(async function(book) {
+            const author = await Author.findById(book.author);
+            book.authorName = author.firstName + " " + author.lastName;
+            return book;
+        }));
+        res.render('lists/show', { title:list.title, list, books: booksWithAuthors})
+    } catch (error) {
+        console.error('Error fetching list:' , error);
+    }
+}
 
 async function create(req, res) {
     req.body.user = req.user._id;
@@ -16,7 +44,6 @@ async function create(req, res) {
     try {
         const list = await List.create(req.body);
         const user = await User.findById(req.user.id);
-        console.log(user);
         if(user.lists) {
             user.lists.push(list._id);
         } else {
--- routes/lists.js
+++ routes/lists.js
@@ -5,7 +5,9 @@ const ensureLoggedIn = require('../config/ensureLoggedIn');
 
 router.get('/:id/lists', ensureLoggedIn, listsCtrl.index);
 router.get('/:id/lists/new', ensureLoggedIn, listsCtrl.new);
+router.get('/:userId/lists/:listId', ensureLoggedIn, listsCtrl.show);
 router.post('/lists', ensureLoggedIn, listsCtrl.create);
+router.post('/:userId/lists/:listId', ensureLoggedIn, listsCtrl.addToList);
 
 
 module.exports = router;
